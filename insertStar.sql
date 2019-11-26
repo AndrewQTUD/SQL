8719c67a432c2ff2356f86f1ebd12e862a38c29c
Andrew Quinn <C15339351@mytudublin.ie>
2019-11-26 12:35:39 +0000

    made changes, still not working

--- insertStar.sql
+++ insertStar.sql
@@ -1,13 +1,17 @@
-DECLARE
-    v_empno copy_emp.EMPLOYEE_ID%TYPE;
-    v_asterisk copy_emp.STAR%TYPE := '';
-    v_sal copy_emp.SALARY%TYPE;
+set verify on
+DECLARE 
+    v_empno copy_emp.employee_id%TYPE := TO_NUMBER(&empno);
+    v_asterisk copy_emp.star%TYPE := NULL;
+    v_sal copy_emp.salary%TYPE;
 BEGIN
     SELECT NVL(ROUND(salary/1000),0) INTO v_sal
-    FROM copy_emp WHERE copy_emp.EMPLOYEE_ID = &p_empno;
-    FOR i IN 1..v_sal 
+    FROM copy_emp WHERE employee_id = v_empno
+    FOR I IN 1..v_sal
     LOOP
-        v_asterisk := v_asterisk||'*';
+        v_asterisk := v_asterisk || '*';
     END LOOP;
 
+    UPDATE copy_emp SET STAR = v_asterisk
+    WHERE employee_id = v_empno;
+    COMMIT;
 END;
