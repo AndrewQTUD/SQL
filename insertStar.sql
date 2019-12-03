a39a44d2776216165d0ff017bd1806a890a571c0
Andrew Quinn <C15339351@mytudublin.ie>
2019-12-03 12:26:15 +0000

    formatting

--- insertStar.sql
+++ insertStar.sql
@@ -1,18 +1,24 @@
-set verify on
+set verify ON 
 DECLARE 
-    v_empno copy_emp.employee_id%TYPE := TO_NUMBER(&empno);
-    v_asterisk copy_emp.star%TYPE := NULL;
-    v_sal copy_emp.salary%TYPE;
-BEGIN
-    SELECT NVL(ROUND(salary/1000),0) INTO v_sal
-    FROM copy_emp WHERE employee_id = v_empno;
-    FOR i IN 1..v_sal
-    LOOP
-        v_asterisk := v_asterisk || '*';
-    END LOOP;
+    v_empno    copy_emp.employee_id%TYPE := To_number(&empno); 
+    v_asterisk copy_emp.star%TYPE := NULL; 
+    v_sal      copy_emp.salary%TYPE; 
+BEGIN 
+    SELECT Nvl(Round(salary / 1000), 0) 
+    INTO   v_sal 
+    FROM   copy_emp 
+    WHERE  employee_id = v_empno; 
 
-    UPDATE copy_emp SET STAR = v_asterisk
-    WHERE employee_id = v_empno;
-    COMMIT;
-END;
-/
+    FOR i IN 1..v_sal LOOP 
+        v_asterisk := v_asterisk 
+                      || '*'; 
+    END LOOP; 
+
+    UPDATE copy_emp 
+    SET    star = v_asterisk 
+    WHERE  employee_id = v_empno; 
+
+    COMMIT; 
+END; 
+
+/ 
--- insertStarCursor.sql
+++ insertStarCursor.sql
@@ -1,22 +1,34 @@
-DECLARE
-v_num_stars number(10);
-v_stars VARCHAR2(50);
-v_num NUMBER(10);
+DECLARE 
+    v_num_stars NUMBER(10); 
+    v_stars     VARCHAR2(50); 
+    v_num       NUMBER(10); 
+BEGIN 
+    FOR emp IN (SELECT employee_id, 
+                       salary 
+                FROM   copy_emp) LOOP 
+        v_num_stars := emp.salary / 1000; 
 
-BEGIN
-    FOR emp in (SELECT employee_id, salary FROM copy_emp) LOOP
-        v_num_stars := emp.salary/1000;
-        v_num_stars := ROUND(v_num_stars);
-        dbms_output.put_line('Number of stars ' || v_num_stars);
-            WHILE v_num_stars > 0 LOOP
-            v_stars := v_stars || '*';
-            v_num_stars := v_num_stars-1;
-            END LOOP;
-        UPDATE copy_emp
-        SET star = v_stars
-        WHERE employee_id = emp.employee_id;
-        v_stars := NULL;
-        v_num_stars := 0;
-    END LOOP;
-END;
-/
+        v_num_stars := Round(v_num_stars); 
+
+        dbms_output.Put_line('Number of stars ' 
+                             || v_num_stars); 
+
+        WHILE v_num_stars > 0 LOOP 
+            v_stars := v_stars 
+                       || '*'; 
+
+            v_num_stars := v_num_stars - 1; 
+        END LOOP; 
+
+        UPDATE copy_emp 
+        SET    star = v_stars 
+        WHERE  employee_id = emp.employee_id; 
+
+        v_stars := NULL; 
+
+        v_num_stars := 0; 
+    END LOOP; 
+END; 
+
+/ 
+/ 
